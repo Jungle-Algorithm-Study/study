8c582b234890c33863c474d87ba4d9cde22022ae
YoungCheon <kakao_story@naver.com>
2022-04-29 21:46:34 +0900

    4/29

---
+++ Week01/youngcheon/크레인인형뽑기/크레인인형뽑기.py
@@ -0,0 +1,22 @@
+def solution(board, moves):
+    baguni = []
+    count = 0
+    for i in moves:
+        for j in range(len(board[0])):
+            if board[j][i-1] != 0:
+                baguni.append(board[j][i-1])
+                board[j][i-1] = 0
+                break
+        if len(baguni)>1 and baguni[-1]==baguni[-2]:
+            print(baguni)
+            baguni = baguni[:-2]
+            print(baguni)
+            count += 2
+    return count
+
+print(solution([[0,0,0,0,0],
+                [0,0,1,0,3],
+                [0,2,5,0,1],
+                [4,2,4,4,2],
+                [3,5,1,3,1]],
+                [1,5,3,5,1,2,1,4]))
