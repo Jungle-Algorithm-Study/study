d680c506e7756150e6de031fae5a9653a66d6176
YoungCheon <kakao_story@naver.com>
2022-05-06 15:41:00 +0900

    수정

--- Week02/youngcheon/오픈채팅방.py
+++ Week02/youngcheon/오픈채팅방.py
@@ -1,10 +1,9 @@
 # 오픈채팅방 
 def solution(record):
     user = {}; message = []
-    for i in record:
-        status, *id_ = i.split()
+    for status, *id_ in map(lambda x: list(x.split()), record):
         if status != 'Change':
             message.append((id_[0], {'Leave':'나갔','Enter':'들어왔'}[status]))
         if status != 'Leave':
-            user[id_[0]]=id_[1]
+            user[id_[0]] = id_[1]
     return [f"{user[m[0]]}님이 {m[1]}습니다." for m in message] 
