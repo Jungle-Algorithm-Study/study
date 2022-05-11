0bfa0b58a02954ad0371c24e666fcb0570ff0638
Dongdongbro <102504535+Dongdongbro@users.noreply.github.com>
2022-05-11 14:05:07 +0900

    Create 폰켓몬.c

---
+++ week02/폰켓몬.c
@@ -0,0 +1,11 @@
+def solution(nums):
+    # answer = 0
+    # pick_pkmon = set(nums)
+    # l_nums = len(nums)//2
+    # l_pick = len(pick_pkmon)
+    # if l_nums < l_pick :
+    #     answer = l_nums
+    # else :
+    #     answer = l_pick
+            
+    return min(len(nums)//2, len(set(nums)))
