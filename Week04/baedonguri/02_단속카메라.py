9b1fb18bf62d9469e899641da4cb8de9935303e2
Baedonguri <71864367+baedonguri@users.noreply.github.com>
2022-05-21 20:26:37 +0900

    Update 02_단속카메라.py

--- Week04/baedonguri/02_단속카메라.py
+++ Week04/baedonguri/02_단속카메라.py
@@ -1,20 +1,34 @@
-def solution(routes):
-    routes.sort(key=lambda x:x[0])
-    routes.sort(key=lambda x:x[1])    
+# def solution(routes):
+#     routes.sort(key=lambda x:x[0])
+#     routes.sort(key=lambda x:x[1])    
 
+#     cnt = 1
+#     f_out = routes[0][1]
+#     flag = True
+
+#     for route in routes[1:]:
+#         s_in, s_out = route        
+#         if s_out >= f_out >= s_in:
+#             flag = False
+#         else:
+#             flag = True
+#             f_out = s_out
+
+#         if flag: cnt += 1
+#     return cnt
+
+# 주형이형 피드백 기반으로 수정한 풀이
+def solution(routes):
+    routes.sort(key=lambda x:x[1])
     cnt = 1
     f_out = routes[0][1]
-    flag = True
 
     for route in routes[1:]:
         s_in, s_out = route        
-        if s_out >= f_out >= s_in:
-            flag = False
-        else:
-            flag = True
+        if not (s_out >= f_out >= s_in):
+            cnt += 1
             f_out = s_out
-
-        if flag: cnt += 1
+        
     return cnt
 
 routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
