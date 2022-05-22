6130758ca80ad2c81f0b13718163ffa1ec4c9e81
YoungCheon <kakao_story@naver.com>
2022-05-22 22:37:37 +0900

    dd

--- Week04/youngcheon/단어변환.py
+++ Week04/youngcheon/단어변환.py
@@ -5,14 +5,14 @@ def solution(begin, target, words):
     check = lambda w1,w2 : sum([1 for a,b in zip(w1,w2) if a==b])+1 == len(w1)
     
     # dfs함수
-    def dfs(word, array, target, count):
+    def dfs(word,count):
         if word == target: #dfs끝까지 도달하면 answer에 append
             answer.append(count)
         for i in range(len(visited)):
         	# 조건을만족하고 visited하지 않은 단어면
             if check(word, words[i]) and not visited[i]:
                 visited[i] = 1
-                dfs(words[i],words,target,count+1)
+                dfs(words[i],count+1)
                 # dfs문이 끝났다는 것은 target에 도달했다는 뜻. 
                 # visited를 다시 미방문 처리(백트래킹)하여 다른 경우의 수 탐색
                 visited[i] = 0 
@@ -20,7 +20,6 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     # dfs 수행
-    dfs(begin, words, target, 0)
+    dfs(begin, 0)
     # 최소값 반환
     return min(answer)
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
