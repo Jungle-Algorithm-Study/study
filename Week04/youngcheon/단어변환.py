d0004be4773b7f7a3fcd8efbc9011a1bfbcebf46
YoungCheon <kakao_story@naver.com>
2022-05-22 20:20:00 +0900

    단변 백트래킹

--- Week04/youngcheon/단어변환.py
+++ Week04/youngcheon/단어변환.py
@@ -1,16 +1,15 @@
-answer = []
-
-check = lambda w1,w2 : sum([1 for a,b in zip(w1,w2) if a==b])+1 == len(w1)
-    
-def dfs(word, array, target, count):
-    if word == target: 
-        return count
-    for i in range(len(array)):
-        if check(word, array[i]):
-            answer.append(dfs(array[i],array[:i]+array[i+1:],target,count+1))
-    return 99999
-
 def solution(begin, target, words):
+    answer = []
+    visited = [0 for _ in range(len(words))]
+    check = lambda w1,w2 : sum([1 for a,b in zip(w1,w2) if a==b])+1 == len(w1)
+    def dfs(word, array, target, count):
+        if word == target: 
+            answer.append(count)
+        for i in range(len(visited)):
+            if check(word, words[i]) and not visited[i]:
+                visited[i] = 1
+                dfs(words[i],words,target,count+1)
+                visited[i] = 0
     if target not in words:
         return 0
     dfs(begin, words, target, 0)
