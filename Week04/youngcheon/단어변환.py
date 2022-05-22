9b9ee07c21d50ef5de5e2420a63e738c95b363fe
YoungCheon <kakao_story@naver.com>
2022-05-22 20:08:37 +0900

    dd

--- Week04/youngcheon/단어변환.py
+++ Week04/youngcheon/단어변환.py
@@ -1,14 +1,13 @@
 answer = []
-def check(w1, w2):
-    return sum([1 for a,b in zip(w1,w2) if a==b])+1 == len(w1)
+
+check = lambda w1,w2 : sum([1 for a,b in zip(w1,w2) if a==b])+1 == len(w1)
     
 def dfs(word, array, target, count):
-    if word == target:
+    if word == target: 
         return count
-    for i in array:
-        if check(word, i):
-            result = dfs(i,array[:array.index(i)]+array[array.index(i)+1:], target, count+1)
-            answer.append(result)
+    for i in range(len(array)):
+        if check(word, array[i]):
+            answer.append(dfs(array[i],array[:i]+array[i+1:],target,count+1))
     return 99999
 
 def solution(begin, target, words):
