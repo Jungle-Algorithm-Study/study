00371b49b47b88058bf6dd62c3106be73a8df0dc
gon2gon2 <ark5354@gmail.com>
2022-05-22 19:57:24 +0900

    큰의미는 없지만 단어변환 리팩터링

--- Week04/gon2gon2/단어변환.py
+++ Week04/gon2gon2/단어변환.py
@@ -2,8 +2,13 @@
 begin에서 target으로 바꾸는 가장 짧은 변환과정
 한 단어만 다른 애로 바꾸고 내가 없는 리스트를 한번 더 탐색
 '''
+
+MAX_VALUE = 99999
+
 def change_possible(foo, bar):
-    ''''''
+    '''
+    단어가 변환가능한지(하나만 다른지) 리턴: True or False
+    '''
     n = len(foo)
     cnt = 0
     for i in range(n):
@@ -17,9 +22,8 @@ def dfs(now, candidate, count, target):
     if now == target:
         return count
     
-    ans = 999999
+    ans = MAX_VALUE
     for i in range(len(candidate)):
-        # 다른 갯수 비교
         if change_possible(now, candidate[i]):
             result = dfs(candidate[i], candidate[:i]+candidate[i+1:], count+1, target)
             ans = min(ans, result)
@@ -31,7 +35,7 @@ def dfs(now, candidate, count, target):
 def solution(begin, target, words):
     if target not in words:
         return 0
-    answer = 99999
+    answer = MAX_VALUE
     answer = min(answer, dfs(begin, words, 0, target))
     
     return answer
