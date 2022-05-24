8ee8d17725deeb7684128191a6fccbf37ecf6d63
YoungCheon <kakao_story@naver.com>
2022-05-24 19:48:23 +0900

    다큰숫

--- Week04/youngcheon/다음큰숫자.py
+++ Week04/youngcheon/다음큰숫자.py
@@ -4,3 +4,8 @@ def solution(n):
         if bin(n+1).count('1') == original:
             return n+1
         n+=1
+# bit 연산
+def solution(n):
+    postPattern = n & -n
+    smallPattern = ((n ^ (n + postPattern)) // postPattern) >> 2
+    return n+postPattern | smallPattern
