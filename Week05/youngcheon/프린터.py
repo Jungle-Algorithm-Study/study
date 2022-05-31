ae76bad74936b1c79a1b6bd02752eca0c0538be2
YoungCheon <kakao_story@naver.com>
2022-06-01 01:31:06 +0900

    .

--- Week05/youngcheon/프린터.py
+++ Week05/youngcheon/프린터.py
@@ -1,14 +1,15 @@
 from collections import deque
 def solution(pri, location):
     answer = []
-    q = deque()
-    for i,e in enumerate(pri):
-        q.append((i, e))
+    q = deque([(i,e) for i,e in enumerate(pri)])
     while q:
-        temp = list(map(lambda x: x[1], q))
+        max_ = max(q, key=lambda x: x[1])[1]
         l, p = q.popleft()
-        if p == max(temp):
+        if p == max_:
             answer.append(l)
         else:
             q.append((l, p))
     return answer.index(location)+1
+
+
+print(solution([2, 1, 3, 2],2))
