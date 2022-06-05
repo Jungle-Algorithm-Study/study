a4b058044e3f45a0aa966cdb43020e8c2c7930ad
YoungCheon <kakao_story@naver.com>
2022-06-05 13:40:19 +0900

    수정

--- Week06/youngcheon/위장.py
+++ Week06/youngcheon/위장.py
@@ -1,10 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 def solution(clothes):
-    d = defaultdict(int)
-    for a,b in clothes:
-        d[b] += 1
-    temp = [v for k,v in d.items()]
-    result = 1
-    for i in temp:
-        result *= i+1
-    return result-1
+    # 옷 종류만 모으기
+    c = Counter(list(map(lambda x: x[1],clothes)))
+    answer = 1
+    # 옷 가짓수 +1를 곱해주면 경우의 수가 나옴
+    # +1을 해주는 이유는 해당 부위를 착용하지 않는 경우를 세기 위해서(투명옷)
+    for v in c.values():
+        answer *= v+1
+    # 아무것도 입지 않은 경우 빼기
+    return answer-1
