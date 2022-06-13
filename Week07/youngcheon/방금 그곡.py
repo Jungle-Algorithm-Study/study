12ac12cae62c1746157a4c46f0aee5dfc31487dc
YoungCheon <kakao_story@naver.com>
2022-06-13 19:58:18 +0900

    song

--- Week07/youngcheon/방금 그곡.py
+++ Week07/youngcheon/방금 그곡.py
@@ -8,12 +8,11 @@ def change_note(note):
 
 def solution(m, musicinfos):
     m = change_note(m)
-    answer = []
+    answer = [["(None)",""]]
     for i in musicinfos:
         info = list(i.split(','))
         sm,ss = map(int,info[0].split(':'))
         em,es = map(int,info[1].split(':'))
-        
         if sm <= em and ss <= es:
             time = (em-sm)*60+(es-ss)
         else:
@@ -23,21 +22,12 @@ def solution(m, musicinfos):
         title = info[2]
         note = change_note(info[3])
         
-        if len(note) < time:
-            # 계산 귀찮아서 걍 1000배 하고 자름.. 효율성은 똥망임
-            note = (note*1000)[:time]
-        else:
-            note = note[:time]
+        # 계산 귀찮아서 걍 1000배 하고 자름.. 효율성은 똥망임
+        note = (note*1000)[:time]
         
         if m in note:
             answer.append([title, note])
     
-    if answer:
-        # 음계 길이만큼 소트하고 거꾸로 뒤집음 (긴 것 먼저 나오게)
-        answer.sort(key = lambda x: len(x[1]), reverse = True)
-        return answer[0][0]
-    else:
-        return "(None)"
-
-print(solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", 
-                          "13:00,13:05,WORLD,ABCDEF"]))
+    # 길이 sort
+    answer.sort(key = lambda x: len(x[1]), reverse = True)
+    return answer[0][0]
