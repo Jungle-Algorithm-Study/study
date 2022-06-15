410388180e9ea8e4b1da61cafebe4ea697f6b1ff
YoungCheon <kakao_story@naver.com>
2022-06-15 19:44:34 +0900

    secret-map

--- Week07/youngcheon/비지밀도.py
+++ Week07/youngcheon/비지밀도.py
@@ -1,5 +1,5 @@
 def solution(n, arr1, arr2):
-    answer = []
+    답 = []
     for i,j in zip(arr1, arr2):
-        answer.append(bin(i|j)[2:].zfill(n).replace('1','#').replace('0',' '))
-    return answer
+        답.append(bin(i|j)[2:].zfill(n).replace('1','#').replace('0',' '))
+    return 답
