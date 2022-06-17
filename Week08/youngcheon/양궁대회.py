70ed23d69b94d16e979a381f2f3e630b1046b558
YoungCheon <kakao_story@naver.com>
2022-06-17 18:50:27 +0900

    양궁

--- Week08/youngcheon/양궁대회.py
+++ Week08/youngcheon/양궁대회.py
@@ -2,42 +2,33 @@
 # end 8:30
 def solution(n, info):
     answer = []
-    
-    # 10,9...1,0 score
     score = list(range(10,-1,-1))
     
-    # (8, 18 test case) reversed score list for priority
-    reverse_score = score[::-1]
-    
-    # dfs함수
+    # dfs함수 정의
     def dfs(n, result, total, index, apeach,priority):
-        if index==10:
+    # n = 화살갯수 / result = 출력리스트/ total = 내 점수
+    # index = 인덱스,종료조건 / apeach = 어피치 점수 / priority = 우선순위(낮은 점수일 수록 높음)
+        apeach_score = score[index] if info[index] else 0
+        if index==10: # 종료조건
+        	# answer 리스트에 저장 ( +[n] 은 남은 화살 다쏘기)
             answer.append([total-apeach,result+[n],priority])
             return
-        elif n <= 0: # 화살이 없을때 (리스트 뒤에 0 채워주기 위함)
-            if info[index]: # 어피치 점수 있을때
-                dfs(n, result+[0], total, index+1, apeach+score[index],priority)
-            else: # 어피치 0점
-                dfs(n, result+[0], total, index+1, apeach,priority)
+        elif n == 0: # 화살이 없을때 (리스트 뒤에 0 채워주기 위함)
+            dfs(n, result+[0], total, index+1, apeach+apeach_score,priority)
         else:
-            for i in range(2):
-                if i == 0: # 두가지 경우의 수 : 쏘지 않고 넘길때,  쏠때
-                    if info[index]: # 나는 쏘지 않는데 어피치는 쐈을때
-                        dfs(n, result+[0], total, index+1, apeach+score[index], priority)
-                    else: # 나도 쏘지 않고 어피치도 쏘지 않을때
-                        dfs(n, result+[0], total, index+1, apeach,priority)
+            for i in range(2): # 두가지 경우의 수 : 쏘지 않고 넘길때, 쏠 때
+                if i == 0: # 쏘지 않고 넘길때
+                    dfs(n, result+[0], total, index+1, apeach+apeach_score, priority)
                 else: # 어피치보다 1발 더쏘기
                     ryan = info[index]+1
                     if n-ryan<0: # 1발 더쏘려고하는데 화살이 부족할때
-                        if info[index]:
-                            dfs(n, result+[0], total, index+1, apeach+score[index], priority)
-                        else:
-                            dfs(n, result+[0], total, index+1, apeach, priority)
-                        return
+                        dfs(n, result+[0],total,index+1,apeach+apeach_score, priority)
                     else:
                         # 어피치보다 한발 더 쏘는경우
                         # result list에 ryan변수만큼 추가, 점수추가, 우선순위점수 추가
-                        dfs(n-ryan,result+[ryan],total+score[index],index+1,apeach, priority+reverse_score[index])
+                        # 화살 갯수 감소 ( n - ryan )
+                        dfs(n-ryan,result+[ryan],total+score[index],index+1,
+                            apeach, priority+score[10-index])
     
     # dfs 실행
     dfs(n, [], 0, 0, 0, 0)
@@ -49,5 +40,6 @@ def solution(n, info):
     if answer[0][0] <= 0:
         return [-1]
     else:
+    	# 내가 이긴 경우
         # result list 리턴
         return answer[0][1]
