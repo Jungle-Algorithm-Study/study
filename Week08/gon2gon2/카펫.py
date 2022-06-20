23cb1a30a37b3e6cadd8323f2a3fe5cae15b5d72
gon2gon2 <ark5354@gmail.com>
2022-06-20 19:54:12 +0900

    카펫 수정

--- Week08/gon2gon2/카펫.py
+++ Week08/gon2gon2/카펫.py
@@ -1,15 +1,16 @@
 def solution(brown, yellow):
     answer = []
+    total_area = brown + yellow
 
     for yh in range(1, (yellow+1) // 2 + 1):
-        
+
         yw, impossible = divmod(yellow, yh)
         if impossible: continue
-        
+
         total_w = yw + 2
         total_h = yh + 2
 
-        if total_w * total_h == brown + yellow:
+        if total_w * total_h == total_area:
             return [total_w, total_h]
 
     return answer
