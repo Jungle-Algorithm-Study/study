576e51020db98cae394a00a49b1b7927fc569775
YoungCheon <kakao_story@naver.com>
2022-06-20 19:22:20 +0900

    카펫

--- Week08/youngcheon/카펫.py
+++ Week08/youngcheon/카펫.py
@@ -1,8 +1,6 @@
 def solution(brown, yellow):
-    brown -= 4
-    brown //= 2
-    
-    for i in range(brown):
-        x, y = i, brown-i
+    n = (brown-4)//2
+    for i in range(n):
+        x, y = i, n-i
         if x*y == yellow:
             return sorted([x+2,y+2],reverse=True)
