ee306d7a4b420d8cf3ab6c4bdb5280b93f330d01
Code-Pilot <102504535+Dongdongbro@users.noreply.github.com>
2022-06-20 19:55:23 +0900

    Update 카펫.py

--- Week08/dongdongbro/카펫.py
+++ Week08/dongdongbro/카펫.py
@@ -1,3 +1,4 @@
+# 나의 이상한 풀이...
 def solution(brown, yellow):
     answer = []
     cnt = brown + yellow
@@ -12,3 +13,15 @@ def solution(brown, yellow):
                         answer.append(j)
                         answer.append(i)
                         return answer
+
+# 수정한 풀이
+def solution(brown, yellow):
+    cnt = brown + yellow
+    
+    for i in range(3, 100000) :
+        if cnt % i != 0 :
+            continue
+        else :
+            j = cnt // i
+            if (i-2) * (j-2) == yellow :
+                return [j,i]
