c3fc925469d419455d7959f6aa22093ec96524f9
gon2gon2 <ark5354@gmail.com>
2022-06-21 19:52:36 +0900

    쿠엥

--- Week08/gon2gon2/이중우선순위큐.py
+++ Week08/gon2gon2/이중우선순위큐.py
@@ -1,34 +1,30 @@
 import heapq
 def solution(operations):
     answer = []
-    
-    min_heap = []
-    max_heap = []
+
+    min_heap, max_heap = [], []
     elem_num = 0
-    
+
     for oper in operations:
         o, n = oper.split()
         n = int(n)
-        
+
         if o == "I":
             elem_num += 1
             heapq.heappush(min_heap, n)
             heapq.heappush(max_heap, -n)
-            
+
         elif o == "D":
             elem_num = max(0, elem_num - 1)
-            
+
             if max_heap and n == 1:
                 x = heapq.heappop(max_heap)
                 min_heap.remove(-x)
-                
+                heapq.heapify(min_heap)
+
             elif min_heap and n == -1:
                 x = heapq.heappop(min_heap)
                 max_heap.remove(-x)
-                
-                
-    if elem_num <= 0:
-        return [0, 0]
-    
-        
-    return [-max_heap[0], min_heap[0]]
+                heapq.heapify(max_heap)
+
+    return [-max_heap[0], min_heap[0]] if elem_num > 0 else [0, 0]
