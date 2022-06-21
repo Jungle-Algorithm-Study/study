c0f729413858a232a17fb134664bf647ca93bf8f
gon2gon2 <ark5354@gmail.com>
2022-06-21 19:43:12 +0900

    이중우선순위큐

---
+++ Week08/gon2gon2/이중우선순위큐.py
@@ -0,0 +1,34 @@
+import heapq
+def solution(operations):
+    answer = []
+    
+    min_heap = []
+    max_heap = []
+    elem_num = 0
+    
+    for oper in operations:
+        o, n = oper.split()
+        n = int(n)
+        
+        if o == "I":
+            elem_num += 1
+            heapq.heappush(min_heap, n)
+            heapq.heappush(max_heap, -n)
+            
+        elif o == "D":
+            elem_num = max(0, elem_num - 1)
+            
+            if max_heap and n == 1:
+                x = heapq.heappop(max_heap)
+                min_heap.remove(-x)
+                
+            elif min_heap and n == -1:
+                x = heapq.heappop(min_heap)
+                max_heap.remove(-x)
+                
+                
+    if elem_num <= 0:
+        return [0, 0]
+    
+        
+    return [-max_heap[0], min_heap[0]]
