5a99ac92630ed3847e8d3be6baf2809d256a2dfc
YoungCheon <kakao_story@naver.com>
2022-06-22 21:04:41 +0900

    feedback

--- Week08/youngcheon/주차.py
+++ Week08/youngcheon/주차.py
@@ -4,16 +4,15 @@ def solution(fees, records):
     dic = defaultdict(int)
     for i in records:
         time, car, status = i.split()
-        temp = list(map(int,time.split(':')))
-        time = temp[0]*60+temp[1]
+        time = int(time[:2])*60+int(time[-2:])
         if status == 'IN':
-            dic[car] += time
-        else:
             dic[car] -= time
-    answer = []
+        else:
+            dic[car] += time
+            
     for k,v in dic.items():
-        fee = v-(23*60+59) if v>=0 else v
-        total = fees[1] + ceil((abs(fee)-fees[0])/fees[2]) * fees[3]
-        answer.append([fees[1] if total < fees[1] else total , k])
-    answer.sort(key= lambda x: x[1])
-    return list(map(lambda x: x[0], answer))
+        fee = v+(23*60+59) if v<=0 else v
+        total = fees[1] + ceil((fee-fees[0])/fees[2]) * fees[3]
+        dic[k] = fees[1] if total < fees[1] else total
+        
+    return [v for k,v in sorted(dic.items())]
