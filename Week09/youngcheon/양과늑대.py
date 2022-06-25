f31456dcdc0c87d42163f9cc2254a30b27f82335
YoungCheon <kakao_story@naver.com>
2022-06-25 20:02:28 +0900

    수정

--- Week09/youngcheon/양과늑대.py
+++ Week09/youngcheon/양과늑대.py
@@ -1,6 +1,5 @@
 def solution(info, edges):
     visited = [0]*len(info)
-    visited2 = [0]*len(edges)
     visited[0] = 1
     answer = []
     def dfs(sheep,wolf):
@@ -12,11 +11,9 @@ def solution(info, edges):
             parent = edges[i][0]
             child = edges[i][1]
             isWolf = info[child]
-            if visited[parent] and not visited2[i]:
+            if visited[parent] and not visited[child]:
                 visited[child] = 1
-                visited2[i] = 1
                 dfs(sheep+(isWolf==0), wolf+(isWolf==1))
                 visited[child] = 0
-                visited2[i] = 0
     dfs(1, 0)
     return max(answer)
