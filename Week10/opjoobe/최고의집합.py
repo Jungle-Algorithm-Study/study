481df9fc398c90a059131ccf4fc6e87a34c3a99b
opjoobe <opjoobe@gmail.com>
2022-07-06 19:11:18 +0900

    최고의집합 숏코딩 (3줄)

--- Week10/opjoobe/최고의집합.py
+++ Week10/opjoobe/최고의집합.py
@@ -1,11 +1,5 @@
 # 프로그래머스 # 최고의 집합 # 5시40분 시작 # 5시57분 끝 # 17min sol # 이걸 ? ? ?
 def solution(n, s):
-    answer = []
-    if n > s:
-        return [-1]
+    if n > s: return [-1]
     magic, rest = divmod(s,n)
-    if rest:
-        answer = [magic] * (n-rest) + [magic+1] * rest
-    else:
-        answer = [magic] * n
-    return answer
+    return [magic] * (n-rest) + [magic+1] * rest if rest else [magic] * n
